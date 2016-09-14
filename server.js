530e24108cb879dc49ea8b4a0115a554d5c3b943
Simon Rodrigues <simonrodri@mweb.co.za>
2016-09-14 12:21:16 +0100

    Added ?completed filter

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.get('/', function(req, res){
 
 // GET /todos
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(_.has(queryParams, 'completed') && (queryParams.completed === 'true' || queryParams.completed === 'false')){
+		var completed = queryParams.completed === 'true' ? true : false;
+		filteredTodos = _.where(filteredTodos, {'completed': completed});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
